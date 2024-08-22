6e1b683ddfdd5e7970cbc20a9f49463233b98ff4
Arslan <themadkid019@gmail.com>
2024-08-22 11:54:12 +0530

    build fix

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/consts_D04Mysqg.mjs
+++
@@ -1,4 +0,0 @@
-const SITE_TITLE = "Dr. Waseem Andrabi";
-const SITE_DESCRIPTION = "IVF Specialist Doctor";
-
-export { SITE_TITLE as S, SITE_DESCRIPTION as a };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/markdown-style-guide - Copy (7)_B8ndpylW.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./markdown-style-guide - Copy (7)_BnR9J_D2.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
